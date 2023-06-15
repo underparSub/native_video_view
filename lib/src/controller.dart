40da4d0914ef6c468483283bb7806d6325104e21
underparSub <92901720+underparSub@users.noreply.github.com>
2023-06-15 19:03:23 +0900

    fix controller seekto

--- lib/src/controller.dart
+++ lib/src/controller.dart
@@ -230,9 +230,9 @@ Future<bool> onPanUpdate(
   /// Must give the [position] of the specific millisecond of playback, if
   /// the [position] is bigger than the duration of source the duration
   /// of the video is used as position.
-  Future<bool> seekTo(int position) async {
+  Future<bool> seekTo(int position, {required bool capture}) async {
     try {
-      Map<String, dynamic> args = {"position": position};
+      Map<String, dynamic> args = {"position": position, "capture": capture};
       await _channel.invokeMethod<void>("player#seekTo", args);
       return true;
     } catch (ex) {
