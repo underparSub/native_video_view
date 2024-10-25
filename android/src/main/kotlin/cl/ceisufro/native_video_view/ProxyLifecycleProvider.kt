103aa02af3fc20d57266b9d18859e991a375182d
underparSub <92901720+underparSub@users.noreply.github.com>
2024-10-25 19:13:04 +0900

    Update ProxyLifecycleProvider.kt

--- android/src/main/kotlin/cl/ceisufro/native_video_view/ProxyLifecycleProvider.kt
+++ android/src/main/kotlin/cl/ceisufro/native_video_view/ProxyLifecycleProvider.kt
@@ -58,7 +58,7 @@ class ProxyLifecycleProvider(activity: Activity) : Application.ActivityLifecycle
         lifecycle.handleLifecycleEvent(Event.ON_DESTROY)
     }
 
-    override fun getLifecycle(): Lifecycle? {
+    override fun getLifecycle(): Lifecycle {
         return lifecycle
     }
 
